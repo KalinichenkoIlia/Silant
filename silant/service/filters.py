e811c2efe621586e4efbd92a19c850f0b0eef90d
TreworReznik <kalinichenko.ilya@gmail.com>
2024-04-19 13:23:11 +0300

    css

--- silant/service/filters.py
+++ silant/service/filters.py
@@ -10,37 +10,37 @@ class CarFilter(django_filters.FilterSet):
         field_name="factory_number",
         label="Зав.номер машины",
         widget=TextInput(
-            attrs={'class': 'form-control'}),
+            attrs={'class': 'form-filter'}),
     )
     model_technique = django_filters.ModelChoiceFilter(
         field_name="model_technique",
         label='Модель техники',
         widget=Select(
-            attrs={'class': 'form-select'}),
+            attrs={'class': 'form-filter'}),
         queryset=models.ModelTechnique.objects.all()
     )
     engine_model = django_filters.ModelChoiceFilter(
         field_name='engine_model',
         label='Модель двигателя',
-        widget=Select(attrs={'class': 'form-select'}),
+        widget=Select(attrs={'class': 'form-filter'}),
         queryset=models.EngineModel.objects.all()
     )
     transmission_model = django_filters.ModelChoiceFilter(
         field_name='transmission_model',
         label='Модель трансмиссии',
-        widget=Select(attrs={'class': 'form-select'}),
+        widget=Select(attrs={'class': 'form-filter'}),
         queryset=models.TransmissionModel.objects.all()
     )
     model_drive_bridge = django_filters.ModelChoiceFilter(
         field_name='model_drive_bridge',
         label='Модель ведущего моста',
-        widget=Select(attrs={'class': 'form-select'}),
+        widget=Select(attrs={'class': 'form-filter'}),
         queryset=models.ModelDriveBridge.objects.all()
     )
     controlled_bridge_model = django_filters.ModelChoiceFilter(
         field_name='controlled_bridge_model',
         label='Модель управляемого моста',
-        widget=Select(attrs={'class': 'form-select'}),
+        widget=Select(attrs={'class': 'form-filter'}),
         queryset=models.ControlledBridgeModel.objects.all()
     )
 
@@ -49,39 +49,39 @@ class TechnicalServiceFilter(django_filters.FilterSet):
     factory_number = django_filters.CharFilter(
         field_name="car__factory_number",
         label="Зав.номер машины",
-        widget=TextInput(attrs={'class': 'form-control'}),
+        widget=TextInput(attrs={'class': 'form-filter'}),
     )
     type_maintenance = django_filters.ModelChoiceFilter(
         field_name='type_maintenance',
         label='Вид ТО',
-        widget=Select(attrs={'class': 'form-select'}),
+        widget=Select(attrs={'class': 'form-filter'}),
         queryset=models.TypeMaintenance.objects.all(),
     )
     date_event = django_filters.DateTimeFilter(
         field_name='date_event',
         label='Дата проведения ТО',
-        widget=DateInput(format='%Y-%m-%d', attrs={'class': 'form-control', 'type': 'date'})
+        widget=DateInput(format='%Y-%m-%d', attrs={'class': 'form-filter', 'type': 'date'})
     )
     order_number = django_filters.CharFilter(
         field_name='order_number',
         label='№ заказ-наряда',
-        widget=TextInput(attrs={'class': 'form-control'}),
+        widget=TextInput(attrs={'class': 'form-filter'}),
     )
     date_order = django_filters.DateTimeFilter(
         field_name='date_order',
         label='Дата заказ-наряда',
-        widget=DateInput(format='%Y-%m-%d', attrs={'class': 'form-control', 'type': 'date'})
+        widget=DateInput(format='%Y-%m-%d', attrs={'class': 'form-filter', 'type': 'date'})
     )
     organization_maintenance = django_filters.ModelChoiceFilter(
         field_name='organization_maintenance',
         label='Организация, проводившая ТО',
-        widget=Select(attrs={'class': 'form-select'}),
+        widget=Select(attrs={'class': 'form-filter'}),
         queryset=models.OrganizationMaintenance.objects.all(),
     )
     service_company = django_filters.ModelChoiceFilter(
         field_name='service_company',
         label='Сервисная компания',
-        widget=Select(attrs={'class': 'form-select'}),
+        widget=Select(attrs={'class': 'form-filter'}),
         queryset=models.ServiceCompany.objects.all(),
     )
 
@@ -90,27 +90,27 @@ class ComplaintsFilter(django_filters.FilterSet):
     car = django_filters.CharFilter(
         field_name="car__factory_number",
         label="Зав.номер машины",
-        widget=TextInput(attrs={'class': 'form-control'}),
+        widget=TextInput(attrs={'class': 'form-filter'}),
     )
     date_refusal = django_filters.DateTimeFilter(
         field_name='date_refusal',
         label='Дата отказа',
-        widget=DateInput(format='%Y-%m-%d', attrs={'class': 'form-control', 'type': 'date'})
+        widget=DateInput(format='%Y-%m-%d', attrs={'class': 'form-filter', 'type': 'date'})
     )
     date_restoration = django_filters.DateTimeFilter(
         field_name='date_restoration',
         label='Дата восстановления',
-        widget=DateInput(format='%Y-%m-%d', attrs={'class': 'form-control', 'type': 'date'})
+        widget=DateInput(format='%Y-%m-%d', attrs={'class': 'form-filter', 'type': 'date'})
     )
     recovery_method = django_filters.CharFilter(
         field_name='recovery_method__title',
         label='Способ восстановления',
-        widget=TextInput(attrs={'class': 'form-control'}),
+        widget=TextInput(attrs={'class': 'form-filter'}),
     )
     service_company = django_filters.ModelChoiceFilter(
         field_name='service_company',
         label='Сервисная компания',
-        widget=Select(attrs={'class': 'form-select'}),
+        widget=Select(attrs={'class': 'form-filter'}),
         queryset=models.ServiceCompany.objects.all(),
     )
 
